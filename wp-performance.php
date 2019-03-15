cca6732718d3dc8b043a066cfe7bd0c5aac35dbb
Vasyl Sokolyk <vsokolyk@gmail.com>
2019-03-15 18:13:06 +0200

    added base info about plugin

--- wp-performance.php
+++ wp-performance.php
@@ -1,5 +1,12 @@
 <?php
 
+/*
+Plugin Name:  wp-performance
+Plugin URI:   https://github.com/Jazz-Man/wp-performance
+Author:       Jazz-Man
+Author URI:   https://github.com/Jazz-Man
+*/
+
 use JazzMan\Performance\App;
 
 new App();
