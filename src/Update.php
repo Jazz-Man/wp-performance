0d1717b41e578075c5a8e1b22bc9bc8c97928b77
Vasyl Sokolyk <vsokolyk@gmail.com>
2019-03-15 18:04:40 +0200

    init Update class

---
+++ src/Update.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace JazzMan\Performance;
+
+/**
+ * Class Update.
+ */
+class Update implements AutoloadInterface
+{
+    public function load()
+    {
+        // TODO: Implement load() method.
+    }
+}
