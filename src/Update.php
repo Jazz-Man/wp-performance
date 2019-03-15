b3381c205e6fd62893a244efc4b0ad5a4a8b52a9
Vasyl Sokolyk <vsokolyk@gmail.com>
2019-03-15 18:39:34 +0200

    Remove bulk action for updating themes/plugins.

--- src/Update.php
+++ src/Update.php
@@ -7,7 +7,6 @@ namespace JazzMan\Performance;
  */
 class Update implements AutoloadInterface
 {
-
     public function load()
     {
         // Stop wp-cron from looking out for new plugin versions
@@ -16,11 +15,20 @@ class Update implements AutoloadInterface
 
         // Prevent users from even trying to update plugins and themes
         add_filter('map_meta_cap', [$this, 'prevent_auto_updates'], 10, 2);
+
+        // Remove bulk action for updating themes/plugins.
+        add_filter('bulk_actions-plugins', [$this, 'remove_bulk_actions']);
+        add_filter('bulk_actions-themes', [$this, 'remove_bulk_actions']);
+        add_filter('bulk_actions-plugins-network', [$this, 'remove_bulk_actions']);
+        add_filter('bulk_actions-themes-network', [$this, 'remove_bulk_actions']);
     }
 
+    /**
+     * Remove all the various places WP does the update checks. As you can see there are a lot of them.
+     */
     public function remove_update_crons()
     {
-        if ( ! App::enabled()) {
+        if (!App::enabled()) {
             return;
         }
 
@@ -53,9 +61,12 @@ class Update implements AutoloadInterface
         remove_action('admin_init', 'wp_auto_update_core');
     }
 
+    /**
+     * Remove all the various schedule hooks for themes, plugins, etc.
+     */
     public function remove_schedule_hook()
     {
-        if ( ! App::enabled()) {
+        if (!App::enabled()) {
             return;
         }
 
@@ -77,16 +88,45 @@ class Update implements AutoloadInterface
     {
         // Check for being enabled and look for specific cap requirements.
         if (App::enabled() && \in_array($cap, [
-                    'install_plugins',
-                    'install_themes',
-                    'update_plugins',
-                    'update_themes',
-                    'update_core',
-                ])) {
+                'install_plugins',
+                'install_themes',
+                'update_plugins',
+                'update_themes',
+                'update_core',
+            ])) {
             $caps[] = 'do_not_allow';
         }
 
         // Send back the data array.
         return $caps;
+    }
+
+    /**
+     * Remove the ability to update plugins/themes from single
+     * site and multisite bulk actions.
+     *
+     * @param array $actions all the bulk actions
+     *
+     * @return array $actions  The remaining actions
+     */
+    public function remove_bulk_actions($actions)
+    {
+        if (App::enabled()) {
+            return $actions;
+        }
+
+        // Set an array of items to be removed with optional filter.
+        if (false === $remove = apply_filters('core_blocker_bulk_items',
+                ['update-selected', 'update', 'upgrade'])) {
+            return $actions;
+        }
+
+        // Loop the item array and unset each.
+        foreach ($remove as $key) {
+            unset($actions[$key]);
+        }
+
+        // Return the remaining.
+        return $actions;
     }
 }
