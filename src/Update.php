1e410c7fe90a5649333442e2dabd2c2f792b0764
Vasyl Sokolyk <vsokolyk@gmail.com>
2019-03-15 19:26:55 +0200

    Disable WordPress from fetching available languages

--- src/Update.php
+++ src/Update.php
@@ -68,6 +68,11 @@ class Update implements AutoloadInterface
 
         // Disable automatic updater updates.
         add_filter('automatic_updater_disabled', '__return_true');
+
+        if (App::enabled()) {
+            // Disable WordPress from fetching available languages
+            add_filter('pre_site_transient_available_translations', [$this, 'available_translations']);
+        }
     }
 
     /**
@@ -384,5 +389,132 @@ class Update implements AutoloadInterface
         }
 
         return $current;
+    }
+
+
+    /**
+     * Returns installed languages instead of all possibly available languages.
+     */
+
+    public function available_translations()
+    {
+        $core_languges = self::core_blocker_get_languages();
+        $installed     = get_available_languages();
+
+        // Call the global WP version.
+        global $wp_version;
+
+        // shared settings
+        $date = date_i18n('Y-m-d H:is', time()); // eg. 2016-06-26 10:08:23
+
+        $available = [];
+
+        foreach ($installed as $lang) {
+            // Try to mimick the data that wordpress puts into 'available_translations' transient
+            $settings = [
+                'language' => $lang,
+                'iso'      => [$lang],
+                'version'  => $wp_version,
+                'updated'  => $date,
+                'strings'  => [
+                    'continue' => __('Continue'),
+                ],
+                'package'  => "https://downloads.wordpress.org/translation/core/{$wp_version}/{$lang}.zip",
+            ];
+
+            $available[$lang] = array_merge($settings, $core_languges[$lang]);
+        }
+
+        return $available;
+    }
+
+    /**
+     * Contains a predefined list of all 4.6 version languages so that we can deduce available languages from languages folder.
+     */
+    private static function core_blocker_get_languages()
+    {
+        return [
+            'ar' => ['english_name' => 'Arabic', 'native_name' => 'العربية'],
+            'ary' => ['english_name' => 'Moroccan Arabic', 'native_name' => 'العربية المغربية'],
+            'az' => ['english_name' => 'Azerbaijani', 'native_name' => 'Azərbaycan dili'],
+            'azb' => ['english_name' => 'South Azerbaijani', 'native_name' => 'گؤنئی آذربایجان'],
+            'bg_BG' => ['english_name' => 'Bulgarian', 'native_name' => 'Български'],
+            'bn_BD' => ['english_name' => 'Bengali', 'native_name' => 'বাংলা'],
+            'bs_BA' => ['english_name' => 'Bosnian', 'native_name' => 'Bosanski'],
+            'ca' => ['english_name' => 'Catalan', 'native_name' => 'Català'],
+            'ceb' => ['english_name' => 'Cebuano', 'native_name' => 'Cebuano'],
+            'cs_CZ' => ['english_name' => 'Czech', 'native_name' => 'Čeština‎'],
+            'cy' => ['english_name' => 'Welsh', 'native_name' => 'Cymraeg'],
+            'da_DK' => ['english_name' => 'Danish', 'native_name' => 'Dansk'],
+            'de_DE_formal' => ['english_name' => 'German (Formal)', 'native_name' => 'Deutsch (Sie)'],
+            'de_DE' => ['english_name' => 'German', 'native_name' => 'Deutsch'],
+            'de_CH_informal' => ['english_name' => '(Switzerland, Informal)', 'native_name' => 'Deutsch (Schweiz, Du)'],
+            'de_CH' => ['english_name' => 'German (Switzerland)', 'native_name' => 'Deutsch (Schweiz)'],
+            'el' => ['english_name' => 'Greek', 'native_name' => 'Ελληνικά'],
+            'en_CA' => ['english_name' => 'English (Canada)', 'native_name' => 'English (Canada)'],
+            'en_ZA' => ['english_name' => 'English (South Africa)', 'native_name' => 'English (South Africa)'],
+            'en_AU' => ['english_name' => 'English (Australia)', 'native_name' => 'English (Australia)'],
+            'en_NZ' => ['english_name' => 'English (New Zealand)', 'native_name' => 'English (New Zealand)'],
+            'en_GB' => ['english_name' => 'English (UK)', 'native_name' => 'English (UK)'],
+            'eo' => ['english_name' => 'Esperanto', 'native_name' => 'Esperanto'],
+            'es_CL' => ['english_name' => 'Spanish (Chile)', 'native_name' => 'Español de Chile'],
+            'es_AR' => ['english_name' => 'Spanish (Argentina)', 'native_name' => 'Español de Argentina'],
+            'es_PE' => ['english_name' => 'Spanish (Peru)', 'native_name' => 'Español de Perú'],
+            'es_MX' => ['english_name' => 'Spanish (Mexico)', 'native_name' => 'Español de México'],
+            'es_CO' => ['english_name' => 'Spanish (Colombia)', 'native_name' => 'Español de Colombia'],
+            'es_ES' => ['english_name' => 'Spanish (Spain)', 'native_name' => 'Español'],
+            'es_VE' => ['english_name' => 'Spanish (Venezuela)', 'native_name' => 'Español de Venezuela'],
+            'es_GT' => ['english_name' => 'Spanish (Guatemala)', 'native_name' => 'Español de Guatemala'],
+            'et' => ['english_name' => 'Estonian', 'native_name' => 'Eesti'],
+            'eu' => ['english_name' => 'Basque', 'native_name' => 'Euskara'],
+            'fa_IR' => ['english_name' => 'Persian', 'native_name' => 'فارسی'],
+            'fi' => ['english_name' => 'Finnish', 'native_name' => 'Suomi'],
+            'fr_BE' => ['english_name' => 'French (Belgium)', 'native_name' => 'Français de Belgique'],
+            'fr_FR' => ['english_name' => 'French (France)', 'native_name' => 'Français'],
+            'fr_CA' => ['english_name' => 'French (Canada)', 'native_name' => 'Français du Canada'],
+            'gd' => ['english_name' => 'Scottish Gaelic', 'native_name' => 'Gàidhlig'],
+            'gl_ES' => ['english_name' => 'Galician', 'native_name' => 'Galego'],
+            'haz' => ['english_name' => 'Hazaragi', 'native_name' => 'هزاره گی'],
+            'he_IL' => ['english_name' => 'Hebrew', 'native_name' => 'עִבְרִית'],
+            'hi_IN' => ['english_name' => 'Hindi', 'native_name' => 'हिन्दी'],
+            'hr' => ['english_name' => 'Croatian', 'native_name' => 'Hrvatski'],
+            'hu_HU' => ['english_name' => 'Hungarian', 'native_name' => 'Magyar'],
+            'hy' => ['english_name' => 'Armenian', 'native_name' => 'Հայերեն'],
+            'id_ID' => ['english_name' => 'Indonesian', 'native_name' => 'Bahasa Indonesia'],
+            'is_IS' => ['english_name' => 'Icelandic', 'native_name' => 'Íslenska'],
+            'it_IT' => ['english_name' => 'Italian', 'native_name' => 'Italiano'],
+            'ja' => ['english_name' => 'Japanese', 'native_name' => '日本語'],
+            'ka_GE' => ['english_name' => 'Georgian', 'native_name' => 'ქართული'],
+            'ko_KR' => ['english_name' => 'Korean', 'native_name' => '한국어'],
+            'lt_LT' => ['english_name' => 'Lithuanian', 'native_name' => 'Lietuvių kalba'],
+            'mk_MK' => ['english_name' => 'Macedonian', 'native_name' => 'Македонски јазик'],
+            'mr' => ['english_name' => 'Marathi', 'native_name' => 'मराठी'],
+            'ms_MY' => ['english_name' => 'Malay', 'native_name' => 'Bahasa Melayu'],
+            'my_MM' => ['english_name' => 'Myanmar (Burmese)', 'native_name' => 'ဗမာစာ'],
+            'nb_NO' => ['english_name' => 'Norwegian (Bokmål)', 'native_name' => 'Norsk bokmål'],
+            'nl_NL' => ['english_name' => 'Dutch', 'native_name' => 'Nederlands'],
+            'nl_NL_formal' => ['english_name' => 'Dutch (Formal)', 'native_name' => 'Nederlands (Formeel)'],
+            'nn_NO' => ['english_name' => 'Norwegian (Nynorsk)', 'native_name' => 'Norsk nynorsk'],
+            'oci' => ['english_name' => 'Occitan', 'native_name' => 'Occitan'],
+            'pl_PL' => ['english_name' => 'Polish', 'native_name' => 'Polski'],
+            'ps' => ['english_name' => 'Pashto', 'native_name' => 'پښتو'],
+            'pt_BR' => ['english_name' => 'Portuguese (Brazil)', 'native_name' => 'Português do Brasil'],
+            'pt_PT' => ['english_name' => 'Portuguese (Portugal)', 'native_name' => 'Português'],
+            'ro_RO' => ['english_name' => 'Romanian', 'native_name' => 'Română'],
+            'ru_RU' => ['english_name' => 'Russian', 'native_name' => 'Русский'],
+            'sk_SK' => ['english_name' => 'Slovak', 'native_name' => 'Slovenčina'],
+            'sl_SI' => ['english_name' => 'Slovenian', 'native_name' => 'Slovenščina'],
+            'sq' => ['english_name' => 'Albanian', 'native_name' => 'Shqip'],
+            'sr_RS' => ['english_name' => 'Serbian', 'native_name' => 'Српски језик'],
+            'sv_SE' => ['english_name' => 'Swedish', 'native_name' => 'Svenska'],
+            'th' => ['english_name' => 'Thai', 'native_name' => 'ไทย'],
+            'tl' => ['english_name' => 'Tagalog', 'native_name' => 'Tagalog'],
+            'tr_TR' => ['english_name' => 'Turkish', 'native_name' => 'Türkçe'],
+            'ug_CN' => ['english_name' => 'Uighur', 'native_name' => 'Uyƣurqə'],
+            'uk' => ['english_name' => 'Ukrainian', 'native_name' => 'Українська'],
+            'vi' => ['english_name' => 'Vietnamese', 'native_name' => 'Tiếng Việt'],
+            'zh_CN' => ['english_name' => 'Chinese (China)', 'native_name' => '简体中文'],
+            'zh_TW' => ['english_name' => 'Chinese (Taiwan)', 'native_name' => '繁體中文'],
+        ];
     }
 }
