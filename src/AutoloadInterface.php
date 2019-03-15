35510a4a3e5d6928c9c87e2969c5013a1d64a176
Vasyl Sokolyk <vsokolyk@gmail.com>
2019-03-15 18:04:14 +0200

    init AutoloadInterface Interface

---
+++ src/AutoloadInterface.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace JazzMan\Performance;
+
+/**
+ * Interface AutoloadInterface
+ *
+ * @package JazzMan\Performance
+ */
+interface AutoloadInterface
+{
+    public function load();
+}
