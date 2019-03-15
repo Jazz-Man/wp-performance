35bf8bd92533a5dfa682c67cf8aee3d330e8752b
Vasyl Sokolyk <vsokolyk@gmail.com>
2019-03-15 18:28:38 +0200

    Stop wp-cron from looking out for new plugin versions

--- src/Update.php
+++ src/Update.php
@@ -9,6 +9,55 @@ class Update implements AutoloadInterface
 {
     public function load()
     {
-        // TODO: Implement load() method.
+        // Stop wp-cron from looking out for new plugin versions
+        add_action('admin_init', [$this, 'remove_update_crons']);
+        add_action('admin_init', [$this, 'remove_schedule_hook']);
+    }
+
+    public function remove_update_crons()
+    {
+        if (!App::enabled()){
+            return;
+        }
+
+        // Disable Theme Updates.
+        remove_action('load-update-core.php', 'wp_update_themes');
+        remove_action('load-themes.php', 'wp_update_themes');
+        remove_action('load-update.php', 'wp_update_themes');
+        remove_action('wp_update_themes', 'wp_update_themes');
+        remove_action('admin_init', '_maybe_update_themes');
+
+        // Disable Plugin Updates.
+        remove_action('load-update-core.php', 'wp_update_plugins');
+        remove_action('load-plugins.php', 'wp_update_plugins');
+        remove_action('load-update.php', 'wp_update_plugins');
+        remove_action('wp_update_plugins', 'wp_update_plugins');
+        remove_action('admin_init', '_maybe_update_plugins');
+
+        // Disable Core updates
+        add_action('init', function () {
+            remove_action('init', 'wp_version_check');
+        }, 2);
+
+        // Don't look for WordPress updates. Seriously!
+        remove_action('wp_version_check', 'wp_version_check');
+        remove_action('admin_init', '_maybe_update_core');
+
+        // Not even maybe.
+        remove_action('wp_maybe_auto_update', 'wp_maybe_auto_update');
+        remove_action('admin_init', 'wp_maybe_auto_update');
+        remove_action('admin_init', 'wp_auto_update_core');
+
+    }
+
+    public function remove_schedule_hook(){
+        if (!App::enabled()){
+            return;
+        }
+
+        wp_clear_scheduled_hook('wp_update_themes');
+        wp_clear_scheduled_hook('wp_update_plugins');
+        wp_clear_scheduled_hook('wp_version_check');
+        wp_clear_scheduled_hook('wp_maybe_auto_update');
     }
 }
