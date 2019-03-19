da64aa7c764dd5de5ba42ea968cd257cd31a2cf3
Vasyl Sokolyk <vsokolyk@gmail.com>
2019-03-19 16:06:22 +0200

    added CleanUp class

--- src/App.php
+++ src/App.php
@@ -46,6 +46,7 @@ class App
             BulkEdit::class,
             TermCount::class,
             Sanitizer::class,
+            CleanUp::class,
         ];
 
         $this->class_autoload_cli = [
@@ -62,11 +63,26 @@ class App
     }
 
     /**
+     * @return bool
+     */
+    public static function is_cron()
+    {
+        return \defined('DOING_CRON') && DOING_CRON;
+    }
+
+    /**
+     * @return bool
+     */
+    public static function is_importing()
+    {
+        return \defined('WP_IMPORTING') && WP_IMPORTING;
+    }
+
+    /**
      * Checks when plugin should be enabled This offers nice compatibilty with wp-cli.
      */
     public static function enabled()
     {
-        return  !(\defined('DOING_CRON') && DOING_CRON) && !self::is_cli() && !(\defined('WP_IMPORTING') && WP_IMPORTING);
+        return  !self::is_cron() && !self::is_cli() && !self::is_importing();
     }
-
 }
