775f7524ac004033535d6a426239faf82a7ee892
Vasyl Sokolyk <vsokolyk@gmail.com>
2019-04-04 17:46:49 +0300

    installed jazzman/autoload-interface

--- src/helper.php
+++ src/helper.php
@@ -1,37 +1,16 @@
 <?php
 
-use JazzMan\Performance\AutoloadInterface;
 use JazzMan\Performance\Shortcode\ShortcodeParser;
 use JazzMan\Performance\Shortcode\ShortcodeRenderer;
 
-if (!function_exists('app_autoload_classes')) {
-    /**
-     * @param array $classes
-     */
-    function app_autoload_classes(array $classes)
-    {
-        foreach ($classes as $class) {
-            try {
-                $_class = new \ReflectionClass($class);
-                if ($_class->implementsInterface(AutoloadInterface::class)) {
-                    /** @var AutoloadInterface $instance */
-                    $instance = $_class->newInstance();
-                    $instance->load();
-                }
-            } catch (\ReflectionException $e) {
-                wp_die($e, 'ReflectionException');
-            }
-        }
-    }
-}
-
-if (!function_exists('app_do_shortcode')){
+if (!function_exists('app_do_shortcode')) {
     /**
      * @param string $content
      *
      * @return string
      */
-    function app_do_shortcode($content){
+    function app_do_shortcode($content)
+    {
         $parser = new ShortcodeParser(false);
         $renderer = new ShortcodeRenderer();
         $doc_tree = $parser->parse($content);
