94158fda68d561a5faf2861aa49cf18e06ac7f94
Vasyl Sokolyk <vsokolyk@gmail.com>
2019-05-20 20:37:25 +0300

    do not load jquery from cdn

--- src/Optimization/Enqueue.php
+++ src/Optimization/Enqueue.php
@@ -13,8 +13,7 @@ class Enqueue implements AutoloadInterface
     public function load()
     {
         add_action('wp_enqueue_scripts', [$this, 'jsToFooter']);
-        add_action('wp_enqueue_scripts', [$this, 'registerJquery'], 100);
-        add_action('wp_head', [$this, 'jqueryLocalFallback']);
+
 
         if (!is_admin()){
             add_filter('script_loader_src', [$this, 'addScriptVersion'], 15, 1);
@@ -48,53 +47,6 @@ class Enqueue implements AutoloadInterface
                     return esc_url($src);
                 }
             }
-        }
-
-        return $src;
-    }
-
-    public function registerJquery()
-    {
-        $jquery_version = wp_scripts()->registered['jquery']->ver;
-
-        wp_deregister_script('jquery');
-
-        wp_register_script(
-            'jquery',
-            "https://code.jquery.com/jquery-{$jquery_version}.min.js",
-            [],
-            null,
-            true
-        );
-
-        add_filter('wp_resource_hints', static function ($urls, $relation_type) {
-            if ('dns-prefetch' === $relation_type) {
-                $urls[] = 'code.jquery.com';
-            }
-
-            return $urls;
-        }, 10, 2);
-
-        add_filter('script_loader_src', [$this, 'jqueryLocalFallback'], 10, 2);
-    }
-
-    /**
-     * @param string      $src
-     * @param string|null $handle
-     *
-     * @return string
-     */
-    public function jqueryLocalFallback($src = '', $handle = null)
-    {
-        static $add_jquery_fallback = false;
-
-        if ($add_jquery_fallback) {
-            echo '<script>(window.jQuery && jQuery.noConflict()) || document.write(\'<script src="'.$add_jquery_fallback.'"><\/script>\')</script>'."\n";
-            $add_jquery_fallback = false;
-        }
-
-        if ('jquery' === $handle) {
-            $add_jquery_fallback = apply_filters('script_loader_src', includes_url('/js/jquery/jquery.js'), 'jquery-fallback');
         }
 
         return $src;
