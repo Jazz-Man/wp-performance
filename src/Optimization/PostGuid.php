851180ef938c218eeb5d07b0de2ee9ef3fb8dbdb
Vasyl Sokolyk <vsokolyk@gmail.com>
2021-07-17 18:03:12 +0300

    replace wp_get_attachment_image_url to app_get_attachment_image_url

--- src/Optimization/PostGuid.php
+++ src/Optimization/PostGuid.php
@@ -8,20 +8,12 @@ use WP_CLI;
 
 class PostGuid implements AutoloadInterface
 {
-
     public function load()
     {
         add_action('save_post', [__CLASS__, 'fixPostGuid'], 10, 2);
     }
 
-    /**
-     * @param  int  $post_id
-     * @param  \WP_Post  $post
-     *
-     * @return void
-     */
-
-    public static function fixPostGuid(int $post_id, \WP_Post $post): void
+    public static function fixPostGuid(int $postId, \WP_Post $post): void
     {
         if (\defined('DOING_AUTOSAVE') && DOING_AUTOSAVE) {
             return;
@@ -29,35 +21,31 @@ class PostGuid implements AutoloadInterface
 
         global $wpdb;
 
-        if ('attachment' === $post->post_type) {
-            $guid = wp_get_attachment_image_url($post_id, 'full');
-        } else {
-            $guid = get_permalink($post_id);
-        }
+        $guid = 'attachment' === $post->post_type ?
+            app_get_attachment_image_url($postId, 'full') :
+            get_permalink($postId);
 
-        if (!empty($guid)) {
-
-            if (App::isCli()){
+        if ( ! empty($guid)) {
+            if (App::isCli()) {
                 WP_CLI::line(
                     sprintf(
                         'Update guid "%s" for post_id "%d" and post_type "%s"',
                         esc_attr($guid),
-                        esc_attr($post_id),
+                        esc_attr($postId),
                         esc_attr($post->post_type)
                     )
                 );
             }
 
-
             $wpdb->update(
                 $wpdb->posts,
                 ['guid' => $guid],
-                ['ID' => $post_id]
+                ['ID' => $postId]
             );
 
-            clean_post_cache($post_id);
+            clean_post_cache($postId);
 
-            _prime_post_caches((array) $post_id, 'attachment' !== $post->post_type);
+            _prime_post_caches((array) $postId, 'attachment' !== $post->post_type);
         }
     }
 }
