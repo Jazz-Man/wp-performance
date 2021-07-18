86fb1caa3096f7f6929432bc53e0e2d9052c23c1
Vasyl Sokolyk <vsokolyk@gmail.com>
2021-07-18 20:31:25 +0300

    move props to local vars

--- src/Security/ContactFormSpamTester.php
+++ src/Security/ContactFormSpamTester.php
@@ -10,38 +10,16 @@ use JazzMan\Performance\App;
  */
 class ContactFormSpamTester implements AutoloadInterface
 {
-    /**
-     * @var string|bool
-     */
-    private $recaptcha_site_key;
-    /**
-     * @var string|bool
-     */
-    private $recaptcha_secret_key;
-
-    /**
-     * @var bool
-     */
-    private $recaptcha_enable;
-
     public function load()
     {
+        $recaptchaEnable = ! App::isCli() && apply_filters('contact_form_recaptcha_enable', true);
 
-        if (App::isCli()) {
-            $this->recaptcha_enable = false;
-        } else {
-            $this->recaptcha_enable = (bool) apply_filters('contact_form_recaptcha_enable', true);
+        if ($recaptchaEnable) {
+            $recaptchaSiteKey = defined('RECAPTCHA_SITE_KEY') ? RECAPTCHA_SITE_KEY : '6LeTc6EUAAAAABNvNwjdbiho6ZatEQdMo5IH7uhd';
+            $recaptchaSecretKey = defined('RECAPTCHA_SECRET_KEY') ? RECAPTCHA_SECRET_KEY : '6LeTc6EUAAAAAD01OZ98Kgv46VdRss7-aEIZ95bA';
+
+            define('WPCF7_RECAPTCHA_SITEKEY', $recaptchaSiteKey);
+            define('WPCF7_RECAPTCHA_SECRET', $recaptchaSecretKey);
         }
-
-        if ($this->recaptcha_enable){
-
-            $this->recaptcha_site_key = \defined('RECAPTCHA_SITE_KEY') ? RECAPTCHA_SITE_KEY : '6LeTc6EUAAAAABNvNwjdbiho6ZatEQdMo5IH7uhd';
-            $this->recaptcha_secret_key = \defined('RECAPTCHA_SECRET_KEY') ? RECAPTCHA_SECRET_KEY : '6LeTc6EUAAAAAD01OZ98Kgv46VdRss7-aEIZ95bA';
-
-            define('WPCF7_RECAPTCHA_SITEKEY',$this->recaptcha_site_key);
-            define('WPCF7_RECAPTCHA_SECRET',$this->recaptcha_secret_key);
-        }
-
     }
-
 }
