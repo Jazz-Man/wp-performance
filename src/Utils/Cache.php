acadf8e53bc5508f82338b3b5810b805a7dd160a
Vasyl Sokolyk <vsokolyk@gmail.com>
2021-07-22 22:50:01 +0300

    refactor naming and phpdoc

--- src/Utils/Cache.php
+++ src/Utils/Cache.php
@@ -3,6 +3,7 @@
 namespace JazzMan\Performance\Utils;
 
 use JazzMan\AutoloadInterface\AutoloadInterface;
+use WP_Term;
 
 class Cache implements AutoloadInterface
 {
@@ -18,57 +19,66 @@ class Cache implements AutoloadInterface
         add_action('wp_create_nav_menu', [$this, 'resetMenuCacheByTermId']);
         add_action('saved_nav_menu', [$this, 'resetMenuCacheByTermId']);
 
-        add_action('save_post_attachment', [$this, 'resetAttachmentCache'], 10, 2);
+        add_action('save_post_attachment', [$this, 'resetAttachmentCache']);
         add_action('saved_term', [$this, 'termsCache'], 10, 3);
     }
 
-    public function resetAttachmentCache(int $post_id, \WP_Post $post)
+    /**
+     * @param  int  $postId
+     * @return void
+     */
+    public function resetAttachmentCache(int $postId): void
     {
-        wp_cache_delete("attachment_image_{$post_id}", self::CACHE_GROUP);
+        wp_cache_delete("attachment_image_$postId", self::CACHE_GROUP);
     }
 
-    public function termsCache(int $term_id, int $tt_id, string $taxonomy)
+    /**
+     * @param  int  $termId
+     * @param  int  $termTaxId
+     * @param  string  $taxonomy
+     *
+     * @SuppressWarnings(PHPMD.UnusedFormalParameter)
+     *
+     * @return void
+     *
+     */
+    public function termsCache(int $termId, int $termTaxId, string $taxonomy): void
     {
-        wp_cache_delete("taxonomy_ancestors_{$term_id}_{$taxonomy}", self::CACHE_GROUP);
-        wp_cache_delete("term_all_children_{$term_id}", self::CACHE_GROUP);
+        wp_cache_delete("taxonomy_ancestors_{$termId}_$taxonomy", self::CACHE_GROUP);
+        wp_cache_delete("term_all_children_$termId", self::CACHE_GROUP);
 
-        app_term_get_all_children($term_id);
+        app_term_get_all_children($termId);
     }
 
-    public function resetMenuCacheByTermId(int $term_id): void
+    public function resetMenuCacheByTermId(int $termId): void
     {
         /** @var \WP_Term $term */
-        $term = get_term($term_id, 'nav_menu');
+        $term = get_term($termId, 'nav_menu');
 
-        if (!empty($term) && !is_wp_error($term)) {
+        if ( $term instanceof WP_Term) {
             self::deleteMenuItemCache($term);
         }
     }
 
     public function resetMenuCacheByMenuId(int $menuId): void
     {
-        /** @var \WP_Term[] $terms */
+        /** @var WP_Term[] $terms */
         $terms = wp_get_post_terms($menuId, 'nav_menu');
 
-        if (!empty($terms) && !is_wp_error($terms)) {
+        if ( ! is_wp_error($terms)) {
             foreach ($terms as $term) {
                 self::deleteMenuItemCache($term);
             }
         }
     }
 
-    private static function deleteMenuItemCache(\WP_Term $item): void
+    private static function deleteMenuItemCache(WP_Term $item): void
     {
         wp_cache_delete(Cache::getMenuItemCacheKey($item), 'menu_items');
     }
 
-    /**
-     * @param  \WP_Term  $item
-     *
-     * @return string
-     */
-    public static function getMenuItemCacheKey(\WP_Term $item): string
+    public static function getMenuItemCacheKey(WP_Term $item): string
     {
-        return "{$item->taxonomy}_{$item->slug}";
+        return "{$item->taxonomy}_$item->slug";
     }
 }
