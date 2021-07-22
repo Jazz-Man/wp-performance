5dbc94752a1837b3dc3814ffc84d43225ac8753a
Vasyl Sokolyk <vsokolyk@gmail.com>
2021-07-22 22:24:55 +0300

    decompose setScriptVersion method

--- src/Optimization/Enqueue.php
+++ src/Optimization/Enqueue.php
@@ -3,7 +3,8 @@
 namespace JazzMan\Performance\Optimization;
 
 use JazzMan\AutoloadInterface\AutoloadInterface;
-use JazzMan\Performance\App;
+use Symfony\Component\WebLink\Link;
+use WP_Site;
 
 /**
  * Class Enqueue.
@@ -16,12 +17,17 @@ class Enqueue implements AutoloadInterface
         add_action('wp_enqueue_scripts', [$this, 'jsToFooter']);
         add_action('wp_enqueue_scripts', [$this, 'jqueryFromCdn']);
 
-        if (!is_admin()) {
+        if ( ! is_admin()) {
             add_filter('script_loader_src', [$this, 'setScriptVersion'], 15, 2);
             add_filter('style_loader_src', [$this, 'setScriptVersion'], 15, 2);
         }
     }
 
+    /**
+     * @param Link[] $links
+     *
+     * @return Link[]
+     */
     public function preloadLinks(array $links): array
     {
         $links[] = Http::dnsPrefetchLink('https://code.jquery.com');
@@ -29,45 +35,55 @@ class Enqueue implements AutoloadInterface
         return $links;
     }
 
-    public function setScriptVersion(string $src, string $handle): string
+    public function setScriptVersion(string $scriptSrc, string $handle): string
     {
-        $isCurrentHost = app_is_current_host($src);
+        if ( ! filter_var($scriptSrc, FILTER_VALIDATE_URL, FILTER_FLAG_PATH_REQUIRED)) {
+            return $scriptSrc;
+        }
 
-        if (\filter_var($src, FILTER_VALIDATE_URL)) {
-            $addVersion = (bool) apply_filters('enqueue_add_script_version', true, $handle);
+        $isCurrentHost = app_is_current_host($scriptSrc);
 
-            if ($addVersion) {
-                if ($isCurrentHost) {
-                    $path = \parse_url($src, PHP_URL_PATH);
-                    $path = \ltrim($path, '/');
-                    $root = app_locate_root_dir();
+        $addVersion = (bool) apply_filters('enqueue_add_script_version', true, $handle);
 
-                    if (is_multisite() && ($blog = get_blog_details(null, false))) {
-                        $path = \ltrim($path, $blog->path);
-                    }
+        if ($addVersion && $isCurrentHost) {
+            $file = self::prepareScriptFilePath($scriptSrc);
 
-                    $file = "{$root}/{$path}";
+            if ($file) {
+                $timestamp = is_readable("$file.map") ? filemtime("$file.map") : filemtime($file);
 
-                    if (\is_readable($file)) {
-                        $timestamp = \is_readable("{$file}.map") ? \filemtime("{$file}.map") : \filemtime($file);
-
-                        $src = add_query_arg([
-                            'ver' => $timestamp,
-                        ], $src);
-
-                        return esc_url($src);
-                    }
-                }
-            } elseif (\strpos($src, '?ver=')) {
-                $src = remove_query_arg('ver', $src);
+                $scriptSrc = add_query_arg([
+                    'ver' => $timestamp,
+                ], $scriptSrc);
             }
+        } elseif (strpos($scriptSrc, '?ver=')) {
+            $scriptSrc = remove_query_arg('ver', $scriptSrc);
         }
 
         if ($isCurrentHost) {
-            return wp_make_link_relative($src);
+            return wp_make_link_relative($scriptSrc);
         }
 
-        return $src;
+        return $scriptSrc;
+    }
+
+    /**
+     * @return false|string
+     */
+    private static function prepareScriptFilePath(string $scriptSrc)
+    {
+        $path = ltrim((string) parse_url($scriptSrc, PHP_URL_PATH), '/');
+
+        if (is_multisite()) {
+            $blogDetails = get_blog_details(null, false);
+
+            $path = $blogDetails instanceof WP_Site ? ltrim($path, $blogDetails->path) : $path;
+        }
+
+        $root = app_locate_root_dir();
+
+        $file = "$root/$path";
+
+        return is_readable($file) ? $file : false;
     }
 
     public static function jqueryFromCdn(): void
@@ -75,7 +91,7 @@ class Enqueue implements AutoloadInterface
         /** @var \_WP_Dependency $jqCore */
         $jqCore = wp_scripts()->registered['jquery-core'];
 
-        $jqVer = \trim($jqCore->ver, '-wp');
+        $jqVer = trim((string) $jqCore->ver, '-wp');
 
         self::deregisterScript($jqCore->handle, sprintf('https://code.jquery.com/jquery-%s.min.js', esc_attr($jqVer)));
         self::deregisterScript('jquery');
@@ -90,36 +106,36 @@ class Enqueue implements AutoloadInterface
         remove_action('wp_head', 'wp_enqueue_scripts', 1);
     }
 
-    public static function deregisterScript(string $handle, string $newUrl = null): void
+    public static function deregisterScript(string $handle, ?string $newUrl = null): void
     {
         $registered = wp_scripts()->registered;
 
-        if (!empty($registered[$handle])) {
+        if ( ! empty($registered[$handle])) {
             /** @var \_WP_Dependency $jsLib */
             $jsLib = $registered[$handle];
 
             wp_dequeue_script($jsLib->handle);
             wp_deregister_script($jsLib->handle);
 
-            if (!empty($newUrl)) {
+            if ( ! empty($newUrl)) {
                 wp_register_script($jsLib->handle, $newUrl, $jsLib->deps, $jsLib->ver, true);
             }
         }
     }
 
-    public static function deregisterStyle(string $handle, string $newUrl = null)
+    public static function deregisterStyle(string $handle, ?string $newUrl = null): void
     {
         $registered = wp_styles()->registered;
 
-        if (!empty($registered[$handle])) {
+        if ( ! empty($registered[$handle])) {
             /** @var \_WP_Dependency $cssLib */
             $cssLib = $registered[$handle];
 
             wp_dequeue_style($cssLib->handle);
             wp_deregister_style($cssLib->handle);
 
-            if (!empty($newUrl)) {
-                wp_register_style($cssLib->handle, $newUrl, $cssLib->deps, $cssLib->ver, $cssLib->args);
+            if ( ! empty($newUrl)) {
+                wp_register_style($cssLib->handle, $newUrl, $cssLib->deps, $cssLib->ver);
             }
         }
     }
