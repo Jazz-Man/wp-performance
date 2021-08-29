4004172c2e7f71f063c4bfcc91d664664d76b057
Vasyl Sokolyk <vsokolyk@gmail.com>
2021-08-29 18:26:22 +0300

    fix MixedAssignment

--- src/Security/ContactFormSpamTester.php
+++ src/Security/ContactFormSpamTester.php
@@ -12,8 +12,10 @@ class ContactFormSpamTester implements AutoloadInterface {
         $recaptchaEnable = ! app_is_wp_cli() && apply_filters('contact_form_recaptcha_enable', true);
 
         if ($recaptchaEnable) {
-            $recaptchaSiteKey = defined('RECAPTCHA_SITE_KEY') ? RECAPTCHA_SITE_KEY : '6LeTc6EUAAAAABNvNwjdbiho6ZatEQdMo5IH7uhd';
-            $recaptchaSecretKey = defined('RECAPTCHA_SECRET_KEY') ? RECAPTCHA_SECRET_KEY : '6LeTc6EUAAAAAD01OZ98Kgv46VdRss7-aEIZ95bA';
+            /** @var string $recaptchaSiteKey */
+        	$recaptchaSiteKey = defined('RECAPTCHA_SITE_KEY') ? RECAPTCHA_SITE_KEY : '6LeTc6EUAAAAABNvNwjdbiho6ZatEQdMo5IH7uhd';
+            /** @var string $recaptchaSecretKey */
+        	$recaptchaSecretKey = defined('RECAPTCHA_SECRET_KEY') ? RECAPTCHA_SECRET_KEY : '6LeTc6EUAAAAAD01OZ98Kgv46VdRss7-aEIZ95bA';
 
             define('WPCF7_RECAPTCHA_SITEKEY', $recaptchaSiteKey);
             define('WPCF7_RECAPTCHA_SECRET', $recaptchaSecretKey);
