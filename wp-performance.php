ed780c2223a45113f8d0206c7eb7aaee8407c6e4
Vasyl Sokolyk <vsokolyk@gmail.com>
2022-03-11 23:04:11 +0200

    update phpdoc comments template

--- wp-performance.php
+++ wp-performance.php
@@ -1,11 +1,11 @@
 <?php
 
-/*
-Plugin Name:  wp-performance
-Plugin URI:   https://github.com/Jazz-Man/wp-performance
-Author:       Jazz-Man
-Author URI:   https://github.com/Jazz-Man
-*/
+/**
+ * Plugin Name:  wp-performance
+ * Plugin URI:   https://github.com/Jazz-Man/wp-performance
+ * Author:       Vasyl Sokolyk
+ * Author URI:   https://www.linkedin.com/in/sokolyk-vasyl.
+ */
 
 use JazzMan\Performance\App;
 
