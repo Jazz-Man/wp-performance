d1f790eb315acc68a2d1e0343e6ec5fba7105d0f
Vasyl Sokolyk <vsokolyk@gmail.com>
2022-03-11 23:32:33 +0200

    fix(psalm): MixedArgument - defined the type of variable for post__in
    Argument 1 of shuffle cannot be non-empty-mixed, expecting array<array-key, mixed>

--- src/Optimization/WPQuery.php
+++ src/Optimization/WPQuery.php
@@ -153,7 +153,13 @@ class WPQuery implements AutoloadInterface {
                     }
 
                     if ( ! empty($wpQuery->query_vars['post__in'])) {
-                        shuffle($wpQuery->query_vars['post__in']);
+						/** @var int[] $postIn */
+						$postIn = $wpQuery->query_vars['post__in'];
+
+						shuffle($postIn);
+
+	                    $wpQuery->set('post__in', $postIn);
+//                        shuffle($wpQuery->query_vars['post__in']);
                     }
 
                     $wpQuery->set('orderby', 'post__in');
