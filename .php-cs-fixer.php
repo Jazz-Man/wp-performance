8b71a121a43f4cac0620cf79c920b99333bb9cdc
Vasyl Sokolyk <vsokolyk@gmail.com>
2022-03-12 23:22:26 +0200

    style: update php-cs-fixer config

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -1,3 +1,8 @@
 <?php
 
-return require_once __DIR__ . '/vendor/jazzman/php-cs-fixer-rules/src/rules-config.php';
+use function JazzMan\PhpCsFixerRules\phpCsFixerConfig;
+
+require_once __DIR__ . '/vendor/jazzman/php-cs-fixer-rules/src/rules-config.php';
+
+
+return phpCsFixerConfig(__DIR__);
